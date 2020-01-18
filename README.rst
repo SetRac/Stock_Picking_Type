89157a05b5d5907c52b66d578d7f421abea00d0b
consulnet <consulnet>
2020-01-17 21:48:16 -0300

    Nuevo Repo

---
+++ README.rst
@@ -0,0 +1,18 @@
+====================
+Herencia en Pagos y arqueo de Caja en ventas
+====================
+
+Usage
+=====
+
+Bug Tracker
+===========
+
+Credits
+=======
+
+Contributors
+------------
+
+* Cesar Chirinos, Consulnet Ltd. 
+
---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Herencia Stock Picking Type',
+    'version': '11.0.0.0.0',
+    'category': 'Stock',
+    'license': 'AGPL-3',
+    'author': 'Cesar Chirinos, Consulnet Ltd.',
+    'website': 'http://consulnet.cl',
+    'maintainer': 'Consulnet Ltd.',
+    'summary': 'Segurudad en Stock Picking Type',
+    'depends': [
+        'stock', "base"
+        ],
+    'data': [
+         'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
